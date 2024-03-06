22a4f1fada3d5cf3399b55a4aa8df70f345e02a9
Chris Yoo <woyoo@umich.edu>
2024-03-06 11:39:04 -0500

    create guest microservice

---
+++ backend/guest/src/main/java/com/chrisyoo/guest/GuestApplication.java
@@ -0,0 +1,11 @@
+package com.chrisyoo.guest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GuestApplication.class, args);
+    }
+}
