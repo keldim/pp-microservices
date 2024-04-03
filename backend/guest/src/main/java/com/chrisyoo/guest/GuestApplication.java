f72409825177c41788b73ceb1202b054f4480b93
Chris Yoo <woyoo@umich.edu>
2024-04-02 23:53:12 -0400

    make guest server be able to send message to kafka

--- backend/guest/src/main/java/com/chrisyoo/guest/GuestApplication.java
+++ backend/guest/src/main/java/com/chrisyoo/guest/GuestApplication.java
@@ -3,7 +3,12 @@ package com.chrisyoo.guest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.chrisyoo.guest",
+                "com.chrisyoo.kafka"
+        }
+)
 public class GuestApplication {
     public static void main(String[] args) {
         SpringApplication.run(GuestApplication.class, args);
