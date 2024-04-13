c95250278ad70f975cd0ff49942008096bb35689
Chris Yoo <woyoo@umich.edu>
2024-04-12 23:45:55 -0400

    trying to send request to ski api after listening to kafka broker

--- backend/notification/src/main/java/com/chrisyoo/config/KafkaListeners.java
+++ backend/notification/src/main/java/com/chrisyoo/config/KafkaListeners.java
@@ -2,11 +2,16 @@ package com.chrisyoo.config;
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class KafkaListeners {
     @KafkaListener(topics = "ski", groupId = "one")
     void listener(String data) {
         System.out.println("Listener received: " + data);
+        String url = "";
+        RestTemplate restTemplate = new RestTemplate();
+        Object[] skiResortList = restTemplate.getForObject(url, Object[].class);
+//        return Arrays.asList(skiResortList);
     }
 }
