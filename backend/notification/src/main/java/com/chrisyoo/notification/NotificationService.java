f51d7bb0b64f1fa62cecb82578bd3995762c0ee1
Chris Yoo <woyoo@umich.edu>
2024-04-14 22:20:27 -0400

    trying to send back the received data from ski api to guest

---
+++ backend/notification/src/main/java/com/chrisyoo/notification/NotificationService.java
@@ -0,0 +1,9 @@
+package com.chrisyoo.notification;
+
+public class NotificationService {
+    public void sendSkiResortList(Object[] skiResortList) {
+        // send it to ski api
+        // but with the sender as guest
+        // or create an endpoint in guest that simply receives list
+    }
+}
