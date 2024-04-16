d2f9247980d3373ae79d6675db0c9d370fc79964
Chris Yoo <woyoo@umich.edu>
2024-04-15 23:53:56 -0400

    write down ideas for what to send back during kafka communication

--- backend/notification/src/main/java/com/chrisyoo/notification/NotificationService.java
+++ backend/notification/src/main/java/com/chrisyoo/notification/NotificationService.java
@@ -5,5 +5,9 @@ public class NotificationService {
         // send it to ski api
         // but with the sender as guest
         // or create an endpoint in guest that simply receives list
+
+        // save the kafka message id as uuid in notification database
+        // in the guest controller, send back the uuid right away after the
+        // kafka message is sent
     }
 }
