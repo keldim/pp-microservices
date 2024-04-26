7a14d49822ecee4b03c0fc61f1ac2563e3c2070a
Chris Yoo <woyoo@umich.edu>
2024-04-25 23:49:49 -0400

    overall flow not working, trying to figure out why

--- backend/notification/src/main/java/com/chrisyoo/config/KafkaConsumerConfig.java
+++ backend/notification/src/main/java/com/chrisyoo/config/KafkaConsumerConfig.java
@@ -1,13 +1,14 @@
 package com.chrisyoo.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
-import org.springframework.kafka.core.*;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 
 import java.util.HashMap;
@@ -16,12 +17,11 @@ import java.util.Map;
 public class KafkaConsumerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
-
     public Map<String, Object> consumerConfig() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return props;
     }
     @Bean
--- backend/notification/src/main/java/com/chrisyoo/config/KafkaListeners.java
+++ backend/notification/src/main/java/com/chrisyoo/config/KafkaListeners.java
@@ -1,41 +1,32 @@
 package com.chrisyoo.config;
 
-import com.chrisyoo.notification.NotificationService;
-import org.json.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 @Component
 public class KafkaListeners {
-    @Autowired
-    NotificationService notificationService;
-    @KafkaListener(topics = "ski", groupId = "one")
+//    @Autowired
+//    NotificationService notificationService;
+    @KafkaListener(topics = "ski", groupId = "list")
     void listener(String uuid) {
         System.out.println("Listener received: " + uuid);
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://ski-resorts-and-conditions.p.rapidapi.com/v1/resort"))
-                .header("X-RapidAPI-Key", "0495933f5emsh19af863bbc666d5p1290dbjsn0dd243175962")
-                .header("X-RapidAPI-Host", "ski-resorts-and-conditions.p.rapidapi.com")
-                .method("GET", HttpRequest.BodyPublishers.noBody())
-                .build();
-
-        String body = "";
-        JSONParser parser = new JSONParser();
-        JSONObject dataInJsonFormat = null;
-        try {
-            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-            body = response.body();
-//            dataInJsonFormat = (JSONObject) parser.parse(body);
-            notificationService.saveSkiResortList(body, uuid);
-        } catch(Exception exception) {
-            System.out.println(exception);
-        }
+// String uuid
+//        HttpRequest request = HttpRequest.newBuilder()
+//                .uri(URI.create("https://ski-resorts-and-conditions.p.rapidapi.com/v1/resort"))
+//                .header("X-RapidAPI-Key", "0495933f5emsh19af863bbc666d5p1290dbjsn0dd243175962")
+//                .header("X-RapidAPI-Host", "ski-resorts-and-conditions.p.rapidapi.com")
+//                .method("GET", HttpRequest.BodyPublishers.noBody())
+//                .build();
+//
+//        String body = "";
+//        JSONParser parser = new JSONParser();
+//        JSONObject dataInJsonFormat = null;
+//        try {
+//            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+//            body = response.body();
+//            notificationService.saveSkiResortList(body, uuid);
+//        } catch(Exception exception) {
+//            System.out.println(exception);
+//        }
     }
 }
