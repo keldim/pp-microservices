c2891ccb16c6c3097bd0590a0e024fb52332dd55
Chris Yoo <woyoo@umich.edu>
2024-05-11 23:48:54 -0400

    still working on producer

--- backend/guest/src/main/java/com/chrisyoo/kafka/KafkaTopicConfig.java
+++ backend/guest/src/main/java/com/chrisyoo/kafka/KafkaTopicConfig.java
@@ -1,15 +1,19 @@
 package com.chrisyoo.kafka;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaTopicConfig {
+
+    @Value("${spring.kafka.topic}")
+    private String topic;
     @Bean
     public NewTopic skiTopic() {
-        return TopicBuilder.name("ski")
+        return TopicBuilder.name(topic)
                 .build();
     }
 }
