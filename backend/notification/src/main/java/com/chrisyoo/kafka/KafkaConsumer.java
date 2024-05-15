66e38e4af5b953615d18fd5399e1f024d654faa1
Chris Yoo <woyoo@umich.edu>
2024-05-14 23:48:45 -0400

    working on consumer

--- backend/notification/src/main/java/com/chrisyoo/kafka/KafkaConsumer.java
+++ backend/notification/src/main/java/com/chrisyoo/kafka/KafkaConsumer.java
@@ -14,7 +14,7 @@ public class KafkaConsumer {
     @KafkaListener(topics = {"ski"})
     void listener(ConsumerRecord<String, String> consumerRecord) {
         log.info("ConsumerRecord : {}", consumerRecord);
-        System.out.println("Listener received: " + uuid);
+        
 // String uuid
 //        HttpRequest request = HttpRequest.newBuilder()
 //                .uri(URI.create("https://ski-resorts-and-conditions.p.rapidapi.com/v1/resort"))
