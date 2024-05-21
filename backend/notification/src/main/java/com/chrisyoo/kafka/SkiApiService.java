257bbe18452e6fcf90b6136828199d02182a3b36
Chris Yoo <woyoo@umich.edu>
2024-05-20 22:53:26 -0400

    refactor request to ski api

---
+++ backend/notification/src/main/java/com/chrisyoo/kafka/SkiApiService.java
@@ -0,0 +1,38 @@
+package com.chrisyoo.kafka;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
+public class SkiApiService {
+
+    public String sendRequestToSkiApi() {
+        String body = "";
+        JSONParser parser = new JSONParser();
+        JSONObject dataInJsonFormat = null;
+        try {
+            HttpResponse<String> response = HttpClient.newHttpClient().send(buildRequest(), HttpResponse.BodyHandlers.ofString());
+            body = response.body();
+        } catch(Exception exception) {
+            System.out.println(exception);
+        }
+
+        return body;
+    }
+
+    private HttpRequest buildRequest() {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://ski-resorts-and-conditions.p.rapidapi.com/v1/resort"))
+                .header("X-RapidAPI-Key", "0495933f5emsh19af863bbc666d5p1290dbjsn0dd243175962")
+                .header("X-RapidAPI-Host", "ski-resorts-and-conditions.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        return request;
+    }
+}
--- backend/notification/src/main/java/com/chrisyoo/kafka/SkiEventsConsumer.java
+++ backend/notification/src/main/java/com/chrisyoo/kafka/SkiEventsConsumer.java
@@ -3,48 +3,28 @@ package com.chrisyoo.kafka;
 import com.chrisyoo.notification.NotificationService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
 @Component
 @Slf4j
 public class SkiEventsConsumer {
-
+    SkiApiService skiApiService;
     NotificationService notificationService;
 
     @Autowired
-    public SkiEventsConsumer(NotificationService notificationService) {
+    public SkiEventsConsumer(SkiApiService skiApiService, NotificationService notificationService) {
+        this.skiApiService = skiApiService;
         this.notificationService = notificationService;
     }
 
     @KafkaListener(topics = {"ski"})
     public void onMessage(ConsumerRecord<String, String> consumerRecord) {
         log.info("ConsumerRecord : {}", consumerRecord);
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://ski-resorts-and-conditions.p.rapidapi.com/v1/resort"))
-                .header("X-RapidAPI-Key", "0495933f5emsh19af863bbc666d5p1290dbjsn0dd243175962")
-                .header("X-RapidAPI-Host", "ski-resorts-and-conditions.p.rapidapi.com")
-                .method("GET", HttpRequest.BodyPublishers.noBody())
-                .build();
-
-        String body = "";
-        JSONParser parser = new JSONParser();
-        JSONObject dataInJsonFormat = null;
-        try {
-            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-            body = response.body();
-            notificationService.saveSkiResortList(body, consumerRecord.value());
-        } catch(Exception exception) {
-            System.out.println(exception);
+        String response = skiApiService.sendRequestToSkiApi();
+        if(!response.equals("")) {
+            notificationService.saveSkiResortList(response, consumerRecord.value());
         }
     }
 }
